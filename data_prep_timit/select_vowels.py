e3f17ac161be13e859ee5287375ba9147aa7d06a
eunjung31 <ej.yeo@snu.ac.kr>
2024-12-30 16:47:48 -0500

    add comments

--- data_prep_timit/select_vowels.py
+++ data_prep_timit/select_vowels.py
@@ -4,7 +4,9 @@ from datasets import load_dataset
 from collections import defaultdict
 from tqdm import tqdm
 import pandas as pd 
+from spk_info import map_spkids
 
+## This script is used to select unique phone list by each speaker.
 def find_unique_phones_by_speaker(path):
     timit = load_dataset("timit_asr", data_dir=path)
     def collect_phones(split):
@@ -31,6 +33,7 @@ def find_unique_phones_by_speaker(path):
     return combined_spk_phoneme
 
 
+## This script is used to select speakers that includes all phonemes that we want to analyze. 
 def select_speakers_with_all_phonemes(combined_spk_phoneme, analysis):
     selected_speakers = []
     for speaker, phones in combined_spk_phoneme.items():
@@ -38,10 +41,10 @@ def select_speakers_with_all_phonemes(combined_spk_phoneme, analysis):
             selected_speakers.append(speaker)
     return selected_speakers
 
-
-def select_analyze_vowels(path, dest, analyze_spks, analyze_phns):
+## This script is used to select and copy files that includes the selected speakers and phonemes.
+def select_analyze_vowels(data_path, path, dest, analyze_spks, analyze_phns):
+    speaker_map = map_spkids(data_path)
     os.makedirs(dest, exist_ok=True)
-    # Walk through the directory tree
     for r, d, files in os.walk(path):
         for fn in tqdm(files):
             speaker = fn.split("_")[1]
@@ -49,7 +52,8 @@ def select_analyze_vowels(path, dest, analyze_spks, analyze_phns):
             if speaker in analyze_spks:
                 if phn in analyze_phns:
                     src_path = os.path.join(r, fn)
-                    speaker_dest_dir = os.path.join(dest, speaker)
+                    nspk = speaker_map[speaker]
+                    speaker_dest_dir = os.path.join(dest, nspk)
                     os.makedirs(speaker_dest_dir, exist_ok=True)
                     dest_path = os.path.join(speaker_dest_dir, fn)
                     shutil.copy(src_path, dest_path)
@@ -58,10 +62,8 @@ def select_analyze_vowels(path, dest, analyze_spks, analyze_phns):
 if __name__ == "__main__":
     path = "/data/user_data/eyeo2/data/timit"
     analysis = "iy ih eh ae ah uw uh ao aa".split()
-    pairs = [("iy", "ih"), ("ih", "eh"), ("eh", "ae"), ("iy", "uw"), ("ih", "uh"), ("eh", "ah"), ("ae", "aa"), ("uw", "uh"), ("uw", "ao"), ("ao","aa")]
-
     combined_spk_phoneme = find_unique_phones_by_speaker(path)
     selected_speakers = select_speakers_with_all_phonemes(combined_spk_phoneme, analysis)
-    select_analyze_vowels(path="/data/user_data/eyeo2/data/CP/all_phone_segments/", dest="/data/user_data/eyeo2/data/CP/analyze_vowels/", analyze_spks=selected_speakers, analyze_phns=analysis)
+    select_analyze_vowels(data_path=path, path="/data/user_data/eyeo2/data/CP/all_phone_segments/", dest="/data/user_data/eyeo2/data/CP/analyze_vowels/", analyze_spks=selected_speakers, analyze_phns=analysis)
 
 
